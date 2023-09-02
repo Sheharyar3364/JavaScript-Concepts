211254f94b1d35ea02ef592b7e5c0e7ec50a7006
Muhammad Sheharyar <96918155+Sheharyar3364@users.noreply.github.com>
2023-09-02 17:31:59 +0000

    discussed conversions in js

---
+++ basics_01/conversion.js
@@ -0,0 +1,38 @@
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2%3);
+// console.log(2**2); // power of something
+// console.log(2**3);
+
+let str1 = "Muhammad Sheharyar";
+let str2 = "Yahya";
+
+// let str3 = str1 + " " + str2;
+// console.log("hello", str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+
+console.log("1" + 2 + 2); // 122
+
+console.log(1 + 2 + "2"); // 32
+
+
+console.log(Boolean("")); // false
+console.log(+""); // 0
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+
+let gameCounter = 100;
+console.log(gameCounter++); // 100
+console.log(gameCounter); // 101
+
+let counter = 99;
+console.log(++counter); // 100
